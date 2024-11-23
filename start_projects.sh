901d4b667d8c46435a491786426fbfc13b697ab8
nikit151102 <nikit15110295@gmail.com>
2024-11-23 13:45:57 +0700

    first commit

---
+++ start_projects.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+BASE_DIR="/путь/к/директории/с/проектами"
+
+PROJECTS=(
+  "AdminConstructionAutomation"
+  "ConstructionAutomationApi"
+  "ConstructionAutomationFront"
+  "ConstructionAutomation.ReportService"
+  "errorLoggingConstruction"
+)
+
+BUILD_MODE=false
+if [[ "$1" == "build" ]]; then
+  BUILD_MODE=true
+  echo "Режим сборки: включен"
+else
+  echo "Режим сборки: выключен"
+fi
+
+start_project() {
+  local project_dir="$1"
+  
+  if [ -f "$project_dir/docker-compose.yml" ]; then
+    echo "Запускаем проект: $project_dir"
+    if $BUILD_MODE; then
+      (cd "$project_dir" && docker-compose up --build -d)
+    else
+      (cd "$project_dir" && docker-compose up -d)
+    fi
+  else
+    echo "Файл docker-compose.yml не найден в $project_dir. Пропускаем."
+  fi
+}
+
+for project in "${PROJECTS[@]}"; do
+  PROJECT_DIR="$BASE_DIR/$project"
+  
+  if [ -d "$PROJECT_DIR" ]; then
+    start_project "$PROJECT_DIR"
+  else
+    echo "Папка $PROJECT_DIR не найдена. Пропускаем."
+  fi
+done
+
+echo "Все проекты обработаны."
