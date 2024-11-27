7339271f810d09520ec32616b581a15bdfbe2269
nikit151102 <nikit15110295@gmail.com>
2024-11-28 02:59:28 +0700

    fix PROJECTS list

--- containers.sh
+++ containers.sh
@@ -1,7 +1,6 @@
 PROJECTS=(
-  "AdminConstructionAutomation"
-  "ConstructionAutomationApi"
-  "ConstructionAutomationFront"
-  "ConstructionAutomation.ReportService"
-  "errorLoggingConstruction"
+    "constructionautomationfront-frontend-1"
+    "constructionautomationapi-backend-1"
+    "constructionautomationreportservice-report_service-1"
+    "telegram-log-service"
 )
